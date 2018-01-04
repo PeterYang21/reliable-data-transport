8a244961d2304ea55a92e154336cdffffa88d8ec
Yiming Yang <yimingyang@dhcp-10-5-18-41.wireless.rochester.edu>
2018-01-04 01:30:30 -0500

    update img

--- rdt_receiver.cc
+++ rdt_receiver.cc
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
--- rdt_sender.cc
+++ rdt_sender.cc
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "rdt_struct.h"
 #include "rdt_sender.h"
